fc2fe5678829c6ab0e26291bd50c43854537c41a
unknown <reynaldo.bjsz@gmail.com>
2022-09-29 17:49:37 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 2000,
+    reset: true,
+})
+
+sr.reveal(`#hero, #banner, #features, #caracteristicas, #cartas, #equipos, #moviles, #precios`, {
+    interval: 200,
+})
